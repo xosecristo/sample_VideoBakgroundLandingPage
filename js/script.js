210ffb9bb69a974f175919147d5a00941555001f
Alonso Valle <xosecristo@outlook.com>
2021-10-13 05:17:58 -0700

    Added: 'about', 'service', & 'contact'

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const nav = document.querySelector('.navigation');
 const menu = document.querySelector('.menu');
 const mainText = document.querySelector('.hero');
 const button = document.querySelector('.btn');
+const circleCntc = document.querySelector('.contact-circle');
 
 gsap.to(mainText, { opacity: '1', duration: 1, x: '0%' });
 gsap.to(button, { opacity: '1', duration: 0.5 }, '-=0');
@@ -25,4 +26,22 @@ function showMenu(e) {
     }
 }
 
+function Showcase() {
+    const a1 = document.querySelector('.showcase_a_1');
+    const b1 = document.querySelector('.showcase_b_1');
+    const PhotosTimeLine = gsap.timeline({ defaults: { duration: 4, ease: 'none', repeat: -1, delay: 1 } });
+    PhotosTimeLine.to(a1, { opacity: 0, yoyo: true });
+    PhotosTimeLine.to(b1, { opacity: 0, yoyo: true });
+}
+
+function showCircle(e) {
+    // console.log(e);
+    if (e.target.classList.contains('contact-circle')) {
+        gsap.to(circleCntc, { opacity: 1, duration: 2 });
+        console.log('miau');
+    }
+}
+
+Showcase();
 menu.addEventListener('click', showMenu);
+window.addEventListener('mouseover', showCircle);
